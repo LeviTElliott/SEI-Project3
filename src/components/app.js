d20f51bfc271609c54604b0b18e80dc8d6e858e4
Levi Elliott <levihackworth@yahoo.com>
2022-05-17 19:58:56 -0500

    update for end of tuesday

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,35 @@
 import React, { useContext } from 'react'
 import { AppContext } from './context'
-
 import Search from './search'
 import Character from './character'
 
-const App = () => {
-  const { charData, error } = useContext(AppContext)
-  console.log(charData)
-  return (
-    <main>
-      <Search />
-      <Character char={charData} error={error} />
-    </main>
-  )
+const characterData =[
+  {name: "", realm: "", race: "", class: "",}
+]
+
+function App() {
+  const allCharacters = characterData.map(
+  (characterData) => {
+  return ( <Character name={characterData.name} realm={characterData.realm} race={characterData.race} class={characterData.class} />)
+})
+return (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      {allCharacters}
+    </header>
+  </div>
+);
 }
 
 export default App
